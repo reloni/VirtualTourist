3f7a78a9315a5430002f4051ef052755315907fd
Anton Efimenko <reloni@ya.ru>
2017-01-29 20:01:31 +0300

    Initial

---
+++ VirtualTourist/MapController.swift
@@ -0,0 +1,51 @@
+//
+//  MapController.swift
+//  VirtualTourist
+//
+//  Created by Anton Efimenko on 29.01.17.
+//  Copyright © 2017 Anton Efimenko. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapController: UIViewController {
+	@IBOutlet weak var mapView: MKMapView!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	@IBAction func editTap(_ sender: Any) {
+	}
+}
+
+extension MapController : MKMapViewDelegate {
+	func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+		if annotation is MKUserLocation {
+			return nil
+		}
+		
+		let pinView: MKPinAnnotationView = {
+			guard let view = mapView.dequeueReusableAnnotationView(withIdentifier: "pin") as? MKPinAnnotationView else {
+				let pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pin")
+				pin.canShowCallout = true
+				pin.animatesDrop = true
+				pin.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+				return pin
+			}
+			return view
+		}()
+		
+		return pinView
+	}
+	
+	func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+	}
+}
