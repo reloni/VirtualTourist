f229bc2c25ad34e4c10442fac4c49a1b8296c34f
Anton Efimenko <reloni@ya.ru>
2017-03-21 21:58:59 +0300

    Add image cell with activity insicator

--- VirtualTourist/DetailController.swift
+++ VirtualTourist/DetailController.swift
@@ -15,9 +15,51 @@ class DetailController: UIViewController {
 	
 	var location: CLLocationCoordinate2D!
 	
+	let sectionInsets = UIEdgeInsets(top: 10.0, left: 15.0, bottom: 10.0, right: 15.0)
+	let itemsPerRow: CGFloat = 3
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
 		mapView.setRegion(MKCoordinateRegion(center: location, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)), animated: false)
+	}
+}
+
+extension DetailController : UICollectionViewDataSource {
+	func numberOfSections(in collectionView: UICollectionView) -> Int {
+		return 1
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 15
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as! ImageCell
+		cell.activityIndicator.startAnimating()
+		return cell
+	}
+}
+
+extension DetailController : UICollectionViewDelegate {
+	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+	}
+}
+
+extension DetailController : UICollectionViewDelegateFlowLayout {
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+		let availableWidth = view.frame.width - paddingSpace
+		let widthPerItem = availableWidth / itemsPerRow
+		
+		return CGSize(width: widthPerItem, height: widthPerItem)
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+		return sectionInsets
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+		return sectionInsets.left
 	}
 }
---
+++ VirtualTourist/ImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCell.swift
+//  VirtualTourist
+//
+//  Created by Anton Efimenko on 21.03.17.
+//  Copyright © 2017 Anton Efimenko. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+	@IBOutlet weak var imageView: UIImageView!
+	@IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+}
